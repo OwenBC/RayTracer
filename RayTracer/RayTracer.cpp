bff47b0f9bbccb69a1c58f4963fe9fdb493ffd82
Owen <ocrewe04@gmail.com>
2022-11-28 21:47:40 -0800

    Collisions and ambient lighting are working Lighting and img plane need work

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -1,69 +1,58 @@
 // RayTracer.cpp 
 
 #include <iostream>
+#include <cmath>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
-#include <stdio.h>
 
-// Output in P6 format, a binary file containing:
-// P6
-// ncolumns nrows
-// Max colour value
-// colours in binary format thus unreadable
-void save_imageP6(int Width, int Height, char* fname, unsigned char* pixels) {
-    FILE* fp;
-    const int maxVal = 255;
+#define FLOATING_POINT_ERROR    0.0001 // Try larger value if this doesn't work
+#define DEPTH                   2
 
-    printf("Saving image %s: %d x %d\n", fname, Width, Height);
-    fp = fopen(fname, "wb");
-    if (!fp) {
-        printf("Unable to open file '%s'\n", fname);
-        return;
-    }
-    fprintf(fp, "P6\n");
-    fprintf(fp, "%d %d\n", Width, Height);
-    fprintf(fp, "%d\n", maxVal);
+void save_imageP6(int Width, int Height, const char* fname, unsigned char* pixels);
+void save_imageP3(int Width, int Height, const char* fname, unsigned char* pixels);
+class Sphere;
+class Light;
 
-    for (int j = 0; j < Height; j++) {
-        fwrite(&pixels[j * Width * 3], 3, Width, fp);
-    }
+// Input file data ----------------------------------
+double near, left, right, top, bottom;
+int nColumns, nRows;
+std::vector<Sphere> sphereList;
+std::vector<Light> lightList;
+std::vector<double> bg_color;
+std::vector<double> ambient_intensity;
+std::string ofilename;
 
-    fclose(fp);
+// Linalg functions ----------------------------------
+
+// Dot product
+double dot(std::vector<double> a, std::vector<double> b) {
+    double sum = 0;
+    for (int i = 0; i < a.size(); i++) sum += a[i] * b[i];
+    return sum;
 }
 
-// Output in P3 format, a text file containing:
-// P3
-// ncolumns nrows
-// Max colour value (for us, and usually 255)
-// r1 g1 b1 r2 g2 b2 .....
-void save_imageP3(int Width, int Height, char* fname, unsigned char* pixels) {
-    FILE* fp;
-    const int maxVal = 255;
-
-    printf("Saving image %s: %d x %d\n", fname, Width, Height);
-    fp = fopen(fname, "w");
-    if (!fp) {
-        printf("Unable to open file '%s'\n", fname);
-        return;
-    }
-    fprintf(fp, "P3\n");
-    fprintf(fp, "%d %d\n", Width, Height);
-    fprintf(fp, "%d\n", maxVal);
-
-    int k = 0;
-    for (int j = 0; j < Height; j++) {
-
-        for (int i = 0; i < Width; i++)
-        {
-            fprintf(fp, " %d %d %d", pixels[k], pixels[k + 1], pixels[k + 2]);
-            k = k + 3;
-        }
-        fprintf(fp, "\n");
-    }
-    fclose(fp);
+// Magnitude
+double mag(std::vector<double> v) {
+    double sum = 0;
+    for (int i = 0; i < v.size(); i++) sum += std::pow(v[i], 2);
+    return std::sqrt(sum);
 }
+
+// Normalize
+void normalize(std::vector<double>& v) {
+    double m = mag(v);
+    for (int i = 0; i < v.size(); i++) v[i] /= m;
+}
+
+// Reflect
+void reflect(std::vector<double>& v, std::vector<double> n) {
+    double coef = 2 * dot(n, v);
+    for (int i = 0; i < 3; i++) v[i] += coef * n[i];
+}
+
+// Geometry Classes ----------------------------------
 
 // Sphere class
 class Sphere
@@ -75,9 +64,55 @@ public:
     std::vector<double> color;
     double Ka, Kd, Ks, Kr;
     int spec;
+private:
+    void transform_ray(std::vector<double>& ray_S, std::vector<double>& ray_c) {
+        for (int i = 0; i < ray_S.size(); i++) {
+            ray_S[i] -= pos[i];
+            ray_S[i] /= scl[i];
+            ray_c[i] /= scl[i];
+        }
+    }
+public:
     Sphere() {};
     ~Sphere() {};
-private:
+    bool intersect(std::vector<double> ray_S, std::vector<double> ray_c, double min_t) {
+        // Inverse transform ray
+        std::vector<double> inv_ray_S(ray_S);
+        std::vector<double> inv_ray_c(ray_c);
+        transform_ray(inv_ray_S, inv_ray_c);
+
+        // Check collision
+        double S_dot_c = dot(inv_ray_S, inv_ray_c);
+        double square_c_mag = std::pow(mag(inv_ray_c), 2);
+        double discriminant = std::pow(S_dot_c, 2) - square_c_mag * (std::pow(mag(inv_ray_S), 2) - 1);
+        if (discriminant > 0) {
+            // Calculate t at intersections and update output if min t on ray
+            double t1 = -(S_dot_c + std::sqrt(discriminant)) / square_c_mag;
+            double t2 = -(S_dot_c - std::sqrt(discriminant)) / square_c_mag;
+            return t1 > min_t || t2 > min_t;
+        }
+        return false;
+    }
+    void intersect(std::vector<double> ray_S, std::vector<double> ray_c, double min_t, double* t, Sphere& out) {
+        // Inverse transform ray
+        std::vector<double> inv_ray_S(ray_S);
+        std::vector<double> inv_ray_c(ray_c);
+        transform_ray(inv_ray_S, inv_ray_c);
+
+        // Check intercept
+        double S_dot_c = dot(inv_ray_S, inv_ray_c); 
+        double square_c_mag = std::pow(mag(inv_ray_c), 2);
+        double discriminant = std::pow(S_dot_c, 2) - square_c_mag * (std::pow(mag(inv_ray_S), 2) - 1);
+        if (discriminant > 0) {
+            // Calculate t at intersections and update output if min t on ray
+            double t1 = -(S_dot_c + std::sqrt(discriminant)) / square_c_mag; 
+            double t2 = -(S_dot_c - std::sqrt(discriminant)) / square_c_mag;
+            if (t1 > min_t)
+                if (t1 < *t || *t == -1) { *t = t1; out = *this; }
+            else if (t2 > min_t)
+                if (t2 < *t || *t == -1) { *t = t2; out = *this; }
+        }
+    }
 };
 
 // Light class
@@ -89,17 +124,79 @@ public:
     std::vector<double> color;
     Light() {};
     ~Light() {};
+    // Check if light is visible from a given position
+    bool visible(std::vector<double> p) {
+        std::vector<double> c(pos);
+        for (int i = 0; i < 3; i++) c[i] -= p[i];
+        normalize(c);
+        for (Sphere sphere : sphereList)
+            if (sphere.intersect(p, c, FLOATING_POINT_ERROR)) return false;
+        return true;
+    }
 private:
 };
 
+// Raytrace function ----------------------------------
 
-std::vector<double> raytrace(std::vector<double> ray_S, std::vector<double> ray_c, int depth_budget) {
+std::vector<double> raytrace(std::vector<double> ray_S, std::vector<double> ray_c, int depth_budget, double min_t) {
+    std::vector<double> color{ 0, 0, 0 };
+
     // Check recursive depth
-    if (depth_budget == 0) {
-        std::vector<double> c(3,0);
-        return c;
-    }
+    if (depth_budget == 0) return color;
 
+    // Collision check
+    double t = -1;
+    Sphere hit_sphere;
+    for (Sphere sphere : sphereList)
+        sphere.intersect(ray_S, ray_c, min_t, &t, hit_sphere);
+
+    // Return background if no collision
+    if (t == -1) return bg_color;
+
+    // Collision point
+    std::vector<double> p_col(ray_S);
+    for (int i = 0; i < 3; i++) p_col[i] += ray_c[i] * t;
+
+    // Normal
+    std::vector<double> norm(p_col);
+    for (int i = 0; i < 3; i++) norm[i] -= hit_sphere.pos[i];
+    normalize(norm);
+
+    // Eye direction vector
+    std::vector<double> eye_dir(ray_c);
+    for (int i = 0; i < 3; i++) eye_dir[i] = -eye_dir[i];
+    normalize(eye_dir);
+
+    // Ambient
+    for (int i = 0; i < 3; i++) color[i] += hit_sphere.Ka * ambient_intensity[i] * hit_sphere.color[i];
+
+    // Diffuse/Specular
+    std::vector<double> l_dir;
+    std::vector<double> l_dir_r;
+    for (Light light : lightList)
+        if (light.visible(p_col)) {
+            // light direction vector
+            l_dir = light.pos;
+            for (int i = 0; i < 3; i++) l_dir[i] -= p_col[i];
+            normalize(l_dir);
+
+            // reflected light direction vector
+            l_dir_r = l_dir;
+            reflect(l_dir_r, norm);
+
+            // Apply light
+            double diff_coef = hit_sphere.Kd * dot(norm, l_dir);
+            double spec_coef = hit_sphere.Ks * std::pow(dot(l_dir_r, eye_dir), hit_sphere.spec);
+            for (int i = 0; i < 3; i++) color[i] += light.color[i] * (diff_coef * hit_sphere.color[i] + spec_coef);
+        }
+
+    // Reflection
+    std::vector<double> ref_c(eye_dir);
+    reflect(ref_c, norm);
+    std::vector<double> ref_color = raytrace(p_col, ref_c, depth_budget - 1, FLOATING_POINT_ERROR); 
+
+    //std::cout << color[0] << ' ' << color[1] << ' ' << color[2] << '\n';
+    return color;
 }
 
 int main(int argc, char* argv[])
@@ -109,15 +206,6 @@ int main(int argc, char* argv[])
         std::cout << "Invalid parameters. Usage: RayTracer.exe <config-file-name>\n";
         return 1;
     }
-
-    // Input file data
-    double near, left, right, top, bottom;
-    int nColumns, nRows;
-    std::vector<Sphere> sphereList;
-    std::vector<Light> lightList;
-    std::vector<double> bg_color;
-    std::vector<double> ambient_intensity;
-    std::string ofilename;
 
     std::cout << "Reading from: " << argv[1] << "\n";
     // Read input file
@@ -218,32 +306,28 @@ int main(int argc, char* argv[])
 
     // Init grid
     unsigned char *pixels;
-    pixels = new unsigned char [3 * nColumns * nRows];
+    pixels = new unsigned char[3 * nColumns * nRows];
 
     /*
-    for (auto i : bg_color) std::cout << i << ' ';
+    for (auto i : sphereList) std::cout << i.name << ' ';
     std::cout << '\n';
-    for (auto i : ambient_intensity) std::cout << i << ' ';
-    std::cout << '\n';
-    std::cout << ofilename << '\n';
     */
 
     std::vector<double> ray_S(3, 0);
+    double width = std::fabs(left - right);
+    double height = std::fabs(bottom - top);
     for (int j = 0; j < nRows; j++) {
         for (int i = 0; i < nColumns; i++) {
-            std::vector<double> ray_c{ (((left + right) * i) / nColumns) - left, (((bottom + top) * j) / nRows) - bottom, -near };
-            std::vector<double> color = raytrace(ray_S, ray_c, 1);
+            std::vector<double> ray_c{ ((width * i) / nColumns) + left, ((height * (nRows - j)) / nRows) + bottom, -near };
+            //std::cout << "Row: " << j << " Col: " << i << '\n';
+            std::vector<double> color = raytrace(ray_S, ray_c, DEPTH, near);
+            pixels[3 * (i + j * nColumns)] = color[0] * 255;
+            pixels[3 * (i + j * nColumns) + 1] = color[1] * 255;
+            pixels[3 * (i + j * nColumns) + 2] = color[2] * 255;
         }
     }
+
+    //std::cout << int(pixels[3 * nColumns * nRows - 1]) << '\n';
+
+    save_imageP6(nColumns, nRows, ofilename.c_str(), pixels);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
